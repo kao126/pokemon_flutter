549053e6026f37ee987b91e56400fdf78b0c3ff6
kao126 <76607249+kao126@users.noreply.github.com>
2025-01-27 22:47:04 +0900

    feat: favorite func

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -4,6 +4,14 @@ import './const/pokeapi.dart';
 import './models/pokemon.dart';
 import './poke_list_item.dart';
 
+class Favorite {
+  final int pokeId;
+
+  Favorite({
+    required this.pokeId,
+  });
+}
+
 class PokeList extends StatefulWidget {
   const PokeList({Key? key}) : super(key: key);
   @override
@@ -11,35 +19,87 @@ class PokeList extends StatefulWidget {
 }
 
 class _PokeListState extends State<PokeList> {
-  static const int more = 30;
-  int pokeCount = more;
+  static const int pageSize = 30;
+  bool isFavoriteMode = true;
+  int _currentPage = 1;
+
+  List<Favorite> favMock = [
+    Favorite(pokeId: 1),
+    Favorite(pokeId: 4),
+    Favorite(pokeId: 7),
+  ];
+
+  // 表示個数
+  int itemCount(int page) {
+    int ret = page * pageSize;
+    if (isFavoriteMode && ret > favMock.length) {
+      ret = favMock.length;
+    }
+    if (ret > pokeMaxId) {
+      ret = pokeMaxId;
+    }
+    return ret;
+  }
+
+  int itemId(int index) {
+    int ret = index + 1; // 通常モード
+    if (isFavoriteMode) {
+      ret = favMock[index].pokeId;
+    }
+    return ret;
+  }
+
+  bool isLastPage(int page) {
+    if (isFavoriteMode) {
+      if (_currentPage * pageSize < favMock.length) {
+        return false;
+      }
+      return true;
+    } else {
+      if (_currentPage * pageSize < pokeMaxId) {
+        return false;
+      }
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<PokemonsNotifier>(
-        builder: (context, pokes, child) => ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-              itemCount: pokeCount + 1, // pokeMaxId,
-              itemBuilder: (context, index) {
-                if (index == pokeCount) {
-                  return OutlinedButton(
-                    child: const Text('more'),
-                    onPressed: () => {
-                      setState(
-                        () {
-                          pokeCount = pokeCount + more;
-                          if (pokeCount > pokeMaxId) {
-                            pokeCount = pokeMaxId;
-                          }
-                        },
-                      )
+    return Column(children: [
+      Container(
+          height: 24,
+          alignment: Alignment.topRight,
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            icon: isFavoriteMode
+                ? const Icon(Icons.star, color: Colors.orangeAccent)
+                : const Icon(Icons.star_outline),
+            onPressed: () => {setState(() => isFavoriteMode = !isFavoriteMode)},
+          )),
+      Expanded(
+          child: Consumer<PokemonsNotifier>(
+              builder: (context, pokes, child) => ListView.builder(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                    itemCount: itemCount(_currentPage) + 1, // pokeMaxId,
+                    itemBuilder: (context, index) {
+                      if (index == itemCount(_currentPage)) {
+                        return OutlinedButton(
+                          onPressed: isLastPage(_currentPage)
+                              ? null
+                              : () => {
+                                    setState(
+                                        () => setState(() => _currentPage++))
+                                  },
+                          child: const Text('more'),
+                        );
+                      } else {
+                        return PokeListItem(
+                          poke: pokes.byId(itemId(index)),
+                        );
+                      }
                     },
-                  );
-                } else {
-                  return PokeListItem(
-                    poke: pokes.byId(index + 1),
-                  );
-                }
-              },
-            ));
+                  )))
+    ]);
   }
 }
