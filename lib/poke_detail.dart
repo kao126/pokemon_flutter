bfd13995e376ad8f68121c66aea4ee8935ba4fca
kao126 <76607249+kao126@users.noreply.github.com>
2025-01-29 23:33:01 +0900

    update: display selected favorite poke item on favorite page

--- lib/poke_detail.dart
+++ lib/poke_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './models/favorite.dart';
 import './models/pokemon.dart';
 import './const/pokeapi.dart';
 
@@ -7,71 +9,83 @@ class PokeDetail extends StatelessWidget {
   final Pokemon poke;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            const Spacer(),
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    poke.imageUrl,
-                    height: 100,
-                    width: 100,
-                  ),
-                ),
-                Container(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(
-                      'No.${poke.id}',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+    return Consumer<FavoritesNotifier>(
+        builder: (context, favs, child) => Scaffold(
+              body: SafeArea(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      leading: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () => Navigator.pop(context),
                       ),
-                    )),
-              ],
-            ),
-            Text(
-              poke.name,
-              style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: poke.types
-                  .map(
-                    (type) => Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Chip(
-                        backgroundColor: pokeTypeColors[type] ?? Colors.grey,
-                        label: Text(
-                          type,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: (pokeTypeColors[type] ?? Colors.grey)
-                                        .computeLuminance() >
-                                    0.5
-                                ? Colors.black
-                                : Colors.white,
-                          ),
-                        ),
+                      trailing: IconButton(
+                        icon: favs.isExist(poke.id)
+                            ? const Icon(Icons.star, color: Colors.orangeAccent)
+                            : const Icon(Icons.star_outline), // <- お気に入りボタン
+                        onPressed: () => {
+                          favs.toggle(Favorite(pokeId: poke.id)),
+                        },
                       ),
                     ),
-                  )
-                  .toList(),
-            ),
-            const Spacer(),
-          ],
-        ),
-      ),
-    );
+                    const Spacer(),
+                    Stack(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(32),
+                          child: Image.network(
+                            poke.imageUrl,
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              'No.${poke.id}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                      ],
+                    ),
+                    Text(
+                      poke.name,
+                      style: const TextStyle(
+                          fontSize: 36, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: poke.types
+                          .map(
+                            (type) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Chip(
+                                backgroundColor:
+                                    pokeTypeColors[type] ?? Colors.grey,
+                                label: Text(
+                                  type,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: (pokeTypeColors[type] ?? Colors.grey)
+                                                .computeLuminance() >
+                                            0.5
+                                        ? Colors.black
+                                        : Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+            ));
   }
 }
